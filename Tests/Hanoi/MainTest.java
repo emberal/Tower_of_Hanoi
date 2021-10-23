a819f4d29f55595516f2f4768c7bfcc2d077fb1d
h600878 <600878@stud.hvl.no>
2021-10-23 17:45:38 +0200

    Moved test files to it's own directory

---
+++ Tests/Hanoi/MainTest.java
@@ -0,0 +1,16 @@
+package Hanoi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void run() {
+    }
+}
